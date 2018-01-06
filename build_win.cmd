38c6f42d0a0bd4874dd6d916cb891656ebcf83ee
Muhammad Farooq <muhammad.farooq@just-eat.com>
2018-01-06 17:16:24 +0000

    Added travis publish

---
+++ build_win.cmd
@@ -0,0 +1,11 @@
+cd src
+cd api
+cd api
+dotnet restore
+dotnet publish --configuration release --self-contained -r win-x86 --output ../bin/dist/win
+cd ..
+cd ..
+cd app
+@IF NOT "%Variable%"=="" npm version %APPVEYOR_BUILD_VERSION% -m 'v%APPVEYOR_BUILD_VERSION
+call npm install 
+call npm run electron:windows
